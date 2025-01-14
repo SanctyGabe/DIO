01ce21058b6a13c2f72e8e324d870d8764d42061
SanctyGabe <gabriel.santoscec@gmail.com>
2025-01-14 09:12:19 -0300

    sim

---
+++ Python Developer/Aulas/03_PROGAMAÇÃO_ORIENTADA_A_OBJETOS/1_Introdução_à_PPO.py
@@ -0,0 +1,44 @@
+class Bicicleta:
+    def __init__(self, cor, modelo, ano, valor):
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano
+        self.valor = valor
+
+    def buzinar(self):
+        print("Plim plim...")
+
+    def parar(self):
+        print("Parando bicicleta...")
+        print("Bicicleta parada!")
+
+    def correr(self):
+        print("Vrummmmm...")
+
+        def acelerando():
+            if correr > self.acelerando:
+                print("Fast af boiiiii...")
+            if correr < self.acelerando:
+                print("Taca-lhe pau neste carrinho, Marcos")
+            else:
+                print("Poker face")
+            
+        # end def
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+
+b1 = Bicicleta("vermelha", "caloi", 2022, 600)
+b1.buzinar()
+b1.correr()
+b1.parar()
+print(b1.cor, b1.modelo, b1.ano, b1.valor)
+
+b2 = Bicicleta("verde", "monark", 2000, 189)
+print(b2)
+b2.correr()
+
+b3 = Bicicleta("roxa", "sport", 2024, 400)
+print(b3)
+b3.buzinar()
